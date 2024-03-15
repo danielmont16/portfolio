e5e6e583208066fb836e6ea5c9d9b5cc1c833d9b
daniel montenegro <dfelipemont@gmail.com>
2024-03-15 13:47:42 -0400

    Fixing carousel behavior

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -13,30 +13,30 @@ const Works = () => (
  <section id="works">
     <h2 className="worksTitle">My Portfolio</h2>
     <span className="worksDesc"> Here are some projects created during Dr. Angela Yu's course and various personal projects. For more details, please visit my GitHub account.</span>
-          <Carousel autoPlay showThumbs={true} infiniteLoop={true}>
+          <Carousel autoPlay showThumbs={true} infiniteLoop={true} centerMode={true} centerSlidePercentage={80} >
             <div>
-              <img className='worksImg'alt="Tindog Page"  src={TindogPage}
-              />
+              <img alt="Tindog Page"  src={TindogPage}/>
               <p className="legend">Tindog Page</p>
+          
             </div>
             <div>
-              <img className='worksImg'alt="Dice Game" src={DiceGame} />
+              <img alt="Dice Game" src={DiceGame} />
               <p className="legend">Dice Game</p>
             </div>
             <div>
-              <img className='worksImg' alt="Simon Game" src={SimonGame} />
+              <img  alt="Simon Game" src={SimonGame} />
               <p className="legend">Simon Game</p>
             </div>
             <div>
-              <img className='worksImg' alt="What-a-font" src={WhataFont} />
+              <img  alt="What-a-font" src={WhataFont} />
               <p className="legend">What a font!</p>
             </div>
             <div>
-              <img  className='worksImg'alt="DrumKit" src={Drumkit} />
+              <img  alt="DrumKit" src={Drumkit} />
               <p className="legend">DrumKit</p>
             </div>
             <div>
-              <img className='worksImg' alt="My favorite candy" src={MyfavoriteCandy} />
+              <img  alt="My favorite candy" src={MyfavoriteCandy} />
               <p className="legend">My favorite Candy</p>
             </div>
 
