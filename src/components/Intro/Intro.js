2b965a895fe70edfe7abfcba89ef8050e3fb2c23
Daniel <dfelipemont@gmail.com>
2024-12-10 12:27:41 -0500

    Intro message modification

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -9,7 +9,7 @@ const Intro = () => {
         <div className='introContent'>
         <span className='hello'>Hello, </span>
         <span className='introText'>I'm <span className='introName'>Daniel</span><br/>Software Developer</span>
-        <p className='introPara'>Computer programming student with strong foundations of software development starting the journey to become a full stack developer,<br/>curious and with willingness to learn new technologies.</p>
+        <p className='introPara'>Web developer enthusiast with a strong foundation in software development, embarking on a journey to become a full-stack developer.<br/>Curious and eager to learn new technologies.</p>
         </div>
         <img src={bg} alt='profile' className='bg'></img>
     </section>
